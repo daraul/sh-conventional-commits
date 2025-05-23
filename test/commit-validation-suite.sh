956c1619937edaa4ebd65ea38272cf7629d83a10
Daraul Harris <thurst306@gmail.com>
2025-05-22 20:08:03 -0400

    test: Test non-empty second line in message
    This commit ensures that a commit message with a non-empty second line
    is rejected.

--- test/commit-validation-suite.sh
+++ test/commit-validation-suite.sh
@@ -5,6 +5,11 @@ testCommitMessageWithNoBodyAccepted() {
     assertEquals 0 $?
 }
 
+testCommitMessageWithInvalidBodyNotAccepted() {
+    output=`main test/res/commit-validation-suite/test-commit-message-with-invalid-body-not-accepted/commit.txt`
+    assertEquals 1 $?
+}
+
 oneTimeSetUp() {
   . ./commit-msg
 }
